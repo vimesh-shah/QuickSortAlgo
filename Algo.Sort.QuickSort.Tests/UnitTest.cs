83fb2c8d04e17a4ddd7b68a70dc9d602f46ffaac
Vimesh Shah <vimesh93@gmail.com>
2022-02-05 11:36:52 +0530

    quick sort algo implemented

---
+++ Algo.Sort.QuickSort.Tests/UnitTest.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace Algo.Sort.QuickSort.Tests;
+
+public class UnitTest
+{
+    [Theory]
+    [InlineData(new int[] { 4, 1, 2, 5, 6, 3 })]
+    [InlineData(new int[] { 3, 1, -3, 0, 2, -2, -1, 4 })]
+    public void AdvancedTest(int[] input)
+    {
+        input.QuickSort();
+
+        int previousItem = int.MinValue;
+
+        foreach (var item in input)
+        {
+            Assert.True(item > previousItem);
+            previousItem = item;
+        }
+    }
+}
---
+++ Algo.Sort.QuickSort/ArrayQuickSort.cs
@@ -0,0 +1,56 @@
+﻿namespace Algo.Sort.QuickSort;
+
+public static class ArrayQuickSort
+{
+    public static void QuickSort(this int[] input)
+    {
+        QuickSort(input, 0, input.Length - 1);
+    }
+
+    private static void QuickSort(int[] input, int left, int right)
+    {
+        if (left >= right)
+        {
+            return;
+        }
+
+        int pivot = input[(left + right) / 2];
+        int index = Partition(input, left, right, pivot);
+
+        QuickSort(input, left, index - 1);
+        QuickSort(input, index, right);
+    }
+
+    private static int Partition(int[] input, int left, int right, int pivot)
+    {
+        while (left <= right)
+        {
+            while (input[left] < pivot)
+            {
+                left++;
+            }
+
+            while (input[right] > pivot)
+            {
+                right--;
+            }
+
+            if (left <= right)
+            {
+                Swap(input, left, right);
+                left++;
+                right--;
+            }
+        }
+
+        return left;
+    }
+
+    private static void Swap(int[] input, int left, int right)
+    {
+        int tmp = input[left];
+        input[left] = input[right];
+        input[right] = tmp;
+    }
+}
+
